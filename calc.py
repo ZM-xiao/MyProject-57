5931586f8e0f4978be30cef6dd8b5d5092d34636
ZM-xiao <3375826785@qq.com>
2025-11-29 09:21:32 +0800

    feat:添加基础计算功能

---
+++ calc.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+if __name__=="__main__":
+    print("加法测试：",add(5,3))
+    print("减法测试：",sub(5,3))
+    print("乘法测试：",mul(5,3))
+
